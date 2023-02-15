f2ba1ce55bce42ef9e3dada84b76bf6115a8ebd1
Rashid-97 <rashidm9797@gmail.com>
2023-02-15 22:52:32 +0400

    Qr Scanner Library Added

---
+++ static/pos/js/warehouse_product.js
@@ -0,0 +1,23 @@
+const scanner = new Html5QrcodeScanner('reader', {
+            // Scanner will be initialized in DOM inside element with id of 'reader'
+            qrbox: {
+                width: 250,
+                height: 250,
+            },  // Sets dimensions of scanning box (set relative to reader element width)
+            fps: 20, // Frames per second to attempt a scan
+        });
+
+
+        scanner.render(success, error);
+        // Starts scanner
+
+        function success(result) {
+            $("#result").html(result)
+            scanner.clear();
+            // Clears scanning instance
+        }
+
+        function error(err) {
+            console.error(err);
+            // Prints any errors to the console
+        }
